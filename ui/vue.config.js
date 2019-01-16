a8a5326c466a0a576368ee9ecbe714c67635f202
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2019-01-16 10:53:44 -0200

    Revert "ui: set baseUrl to /"
    This reverts commit 95e142f8d976ebb19c91764d4aa254d49d0474a0.

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,7 +1,7 @@
 var ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-    baseUrl: '/',
+    baseUrl: '/ui',
     configureWebpack: {
         plugins: [
             new ManifestPlugin()
